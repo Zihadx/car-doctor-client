e74e719e532081963850cd39b7a9d9cbeed90901
zihadx <blankzihad@gmail.com>
2023-05-12 23:29:35 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    themes: false,
+  },
+  daisyui: {
+    themes: false,
+  },
+  plugins: [require("daisyui")],
+  
+}
+
